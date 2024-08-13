96b53fe79b48ba83570cd53a0e7f37441d00d796
Joshua Olajide <joshua@resilience17.com>
2024-08-13 02:12:03 +0100

    chore: add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import { DevfestLogo } from '@/components/icons';
-import { Button } from "@/components/ui/button"
+import { DevfestLogo } from '@/app/components/icons';
+import { Button } from '@/app/components/ui/button';
 import { caleit } from './layout';
 
 export default function Home() {
   return (
     <main className="flex gap-5 flex-col items-center justify-between p-24">
-      <h1 className={`text-3xl ${caleit.className}`} >Devfest Ibadan '24</h1>
+      <h1 className={`text-3xl ${caleit.className}`}>
+        Devfest Ibadan &apos;24
+      </h1>
       <DevfestLogo />
       <Button size={'lg'}>Proceed</Button>
     </main>
