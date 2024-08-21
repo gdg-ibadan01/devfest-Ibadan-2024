3c54c0bf99160acc89a01027902d47a380e57268
Joshua Olajide <joshua@resilience17.com>
2024-08-21 01:46:47 +0100

    feat: update with classes and types

---
+++ app/components/menulink/menulink.classes.ts
@@ -0,0 +1,7 @@
+import { TMenuPropsType } from '@/types/menu-props-type';
+
+export default <TMenuPropsType>{
+  link: 'text-center my-4',
+  list: 'w-full cursor-pointer hover:font-semibold sm:text-xs lg:text-sm xl:text-base block p-8 lg:p-0 transition-colors duration-100 ease-in',
+  // Update Type and keep adding more styles
+};
