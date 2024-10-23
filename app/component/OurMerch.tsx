7109ab7875de3de9eabf34e32a8c301e6085ce9a
Semight <davidoluwasemiloorei@gmail.com>
2024-10-23 15:47:48 +0100

    feat(layout): added smooth scroll to all the pages

--- app/component/OurMerch.tsx
+++ app/component/OurMerch.tsx
@@ -33,7 +33,7 @@ const OurMerch = () => {
                 Get Now <ArrowUpRight />
               </Link>
             </div>
-            <div className="w-full flex flex-col md:flex-row gap-24 z-10 md:pl-[2rem] md:pr-[6rem]">
+            <div className="w-full flex flex-col md:flex-row gap-24 z-10">
               <Image
                 src={backpack}
                 alt="backpack"
