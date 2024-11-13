7c1f12135686a3f3b543d0bd1732227e8ad616f1
Semight <davidoluwasemiloorei@gmail.com>
2024-11-13 23:21:29 +0100

    fix(agenda): modified agenda page

--- app/agenda/agenda.ts
+++ app/agenda/agenda.ts
@@ -20,9 +20,9 @@ export const agendaClass = {
   accordionItem: 'px-[10px] lg:px-[38px] border-y border-x-2 border-[#1E1E1E]',
   accordionContentContainer: 'mb-[30px] flex flex-col gap-[35px]',
   accordionTrigger:
-    'mb-5 font-bold text-[15px] lg:text-[20px] hover:no-underline',
+    'mb-5 font-bold text-[15px] text-left lg:text-[20px] hover:no-underline',
   accordionContent: 'flex flex-row items-center justify-between lg:gap-0 gap-5',
-  accordionContentTitle: 'text-[15px] lg:text-[20px] lg:w-auto w-[80%]',
+  accordionContentTitle: 'text-[15px] lg:text-[19px] lg:w-auto w-[80%]',
   accordionContentButton:
-    'border rounded-[63px] text-white border-[accordion-item-border] w-[80px] lg:w-[60px] h-[48px]',
+    'border rounded-[22px] text-white border-[accordion-item-border] w-[80px] lg:w-[85px] h-[35px]',
 };
--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -38,158 +38,170 @@ export default function Agenda() {
     {
       activity: 'Registrations and Arrival',
       time: '8:30 am - 9:00 am',
-      duration: '30 mins',
-      details: ['Hall: Gemini'],
+      title: ['Registrations and arrival'],
+      details: ['Hall Gemini'],
     },
     {
       activity: 'Introduction/Opening',
       time: '9:00 am - 9:15 am',
-      duration: '15 mins',
+      title: ['Introduction/Opening'],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Speaker Session 1 - Dr. Olusola Ayoola (Group Mailing)',
       time: '9:20 am - 9:45 am',
-      duration: '25 mins',
+      title: ['Speaker Session 1 - Dr. Olusola Ayoola (Group Mailing)'],
       details: ['Hall Gemini'],
     },
     {
       activity:
         'Speaker Session 2 - Esther Ubeng (Creating valuable products that captivate and retain users)',
       time: '9:50 am - 10:15 am',
-      duration: '25 mins',
+      title: [
+        'Speaker Session 2 - Esther Ubeng (Creating valuable products that captivate and retain users)',
+      ],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Keynote Speech 1',
       time: '10:20 am - 10:50 am',
-      duration: '30 mins',
+      title: ['Keynote Speech 1'],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Product Demo (Sponsors)',
       time: '10:50 am - 11:00 am',
-      duration: '10 mins',
+      title: ['Product Demo (Sponsors)'],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Networking/Break',
       time: '11:00 am - 11:20 am',
-      duration: '20 mins',
+      title: ['Networking/Break'],
       details: ['Hall Gemini'],
     },
     {
       activity:
         'Speaker Session 3 - Feranmi Olowoyo (The Impact of Community on Developer Career Growth)',
       time: '11:20 am - 11:45 am',
-      duration: '25 mins',
+      title: [
+        'Speaker Session 3 - Feranmi Olowoyo (The Impact of Community on Developer Career Growth)',
+      ],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Breakout Workshop 1 & 2 & 3',
       time: '11:55 am - 12:35 pm',
-      duration: '40 mins',
-      details: [
-        'Hall Gemini - John O. Ajeigbe (Next.js + Gemini AI: Building a Real-Time News Analysis and Fact-Checking Engine)',
-        'Hall Gamma - Oluwole Adebiyi (Leveraging Firebase for Scalable Real-Time Applications: A Smarter Alternative to WebSockets)',
-        'Hall Vertex - Tunmise Ogunniyi (Serverless 101 with Google Cloud Functions)',
+      title: [
+        'John O. Ajeigbe - (Next.js + Gemini AI: Building a Real-Time News Analysis and Fact-Checking Engine)',
+        'Oluwole Adebiyi - (Leveraging Firebase for Scalable Real-Time Applications: A Smarter Alternative to WebSockets)',
+        'Tunmise Ogunniyi - (Serverless 101 with Google Cloud Functions)',
       ],
+      details: ['Hall Gemini', 'Hall Gamma', 'Hall Vertex'],
     },
     {
       activity: 'Breakout Workshop 4 & 5 & 6',
       time: '12:40 pm - 01:20 pm',
-      duration: '40 mins',
-      details: [
-        'Hall Vertex - Mustapha Adekunle (Vertex AI Embeddings API + Vector Search Grounding LLMs made easy)',
-        'Hall Gemini - Rebecca Saka (Exploring Dart Beyond Flutter: Building CLI Applications and Best Practices)',
-        'Hall Gamma - Motunrayo Koyejo (Building Scalable Backend Systems: From Prototype to Production)',
+      title: [
+        'Mustapha Adekunle - (Vertex AI Embeddings API + Vector Search Grounding LLMs made easy)',
+        'Rebecca Saka - (Exploring Dart Beyond Flutter: Building CLI Applications and Best Practices)',
+        'Motunrayo Koyejo - (Building Scalable Backend Systems: From Prototype to Production)',
       ],
+      details: ['Hall Vertex', 'Hall Gemini', 'Hall Gamma'],
     },
     {
       activity: 'Breakout Speaker Sessions 4 & 5 & 6',
       time: '01:25 pm - 01:50 pm',
-      duration: '25 mins',
+      title: ['Breakout Speaker Sessions 4 & 5 & 6'],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Pictures / Item 7',
       time: '01:50 pm - 02:20 pm',
-      duration: '30 mins',
+      title: ['Pictures / Item 7'],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Panel Session',
       time: '02:25 pm - 03:10 pm',
-      duration: '45 mins',
-      details: [
+      title: [
         'Ahmed Olanrewaju',
         'Israel Odeajo',
         'Aminat Akinyemi Ayoola',
         'Kingsley Owadara',
       ],
+      details: ['Hall Gemini'],
     },
     {
       activity:
         'Lightning Talk 1 - Opeyimika Aremu (From Scrubs to Scripts: Mastering Tech Skills While Life Happens)',
       time: '03:10 pm - 03:20 pm',
-      duration: '10 mins',
+      title: [
+        'Opeyimika Aremu - (From Scrubs to Scripts: Mastering Tech Skills While Life Happens)',
+      ],
       details: ['Hall Gemini'],
     },
     {
       activity:
         'Lightning Talk 2 - Great Oloyede (Agile Devs, Agile Lives: Project Management Hacks for Tech Professionals)',
       time: '03:20 pm - 03:30 pm',
-      duration: '10 mins',
+      title: [
+        'Great Oloyede - (Agile Devs, Agile Lives: Project Management Hacks for Tech Professionals)',
+      ],
       details: ['Hall Gemini'],
     },
     {
       activity:
         'Lightning Talk 3 - Makinde Daniel (Technical Interview Prep: Tips, Tricks, and Winning Strategies)',
       time: '03:30 pm - 03:40 pm',
-      duration: '10 mins',
+      title: [
+        'Makinde Daniel (Technical Interview Prep: Tips, Tricks, and Winning Strategies)',
+      ],
       details: ['Hall Gemini'],
     },
     {
       activity:
         "Lightning Talk 4 - Temitope Aiyegbusi (Embrace Curiosity: The Power of Questioning and Adopting a Beginner's Mindset for Continuous Growth)",
       time: '03:40 pm - 03:50 pm',
-      duration: '10 mins',
+      title: [
+        "Temitope Aiyegbusi (Embrace Curiosity: The Power of Questioning and Adopting a Beginner's Mindset for Continuous Growth)",
+      ],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Product Demo (Sponsors)',
       time: '03:50 pm - 04:00 pm',
-      duration: '10 mins',
+      title: ['Product Demo (Sponsors)'],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Organizer’s Speech/Presenting the Organizing Team',
       time: '04:00 pm - 04:10 pm',
-      duration: '10 mins',
+      title: ['Organizer’s Speech/Presenting the Organizing Team'],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Hackathon Presentation',
       time: '04:15 pm - 04:40 pm',
-      duration: '25 mins',
+      title: ['Hackathon Presentation'],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Games',
       time: '04:40 pm - 05:00 pm',
-      duration: '20 mins',
+      title: ['Games'],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Gift Presentation',
       time: '05:00 pm - 05:10 pm',
-      duration: '10 mins',
+      title: ['Gift Presentation'],
       details: ['Hall Gemini'],
     },
     {
       activity: 'Vote of Thanks',
       time: '05:10 pm - 05:20 pm',
-      duration: '10 mins',
+      title: ['Vote of Thanks'],
       details: ['Hall Gemini'],
     },
   ];
@@ -218,28 +230,48 @@ export default function Agenda() {
             collapsible
             className="max-w-[1000px] mx-auto"
           >
-            {agendaItems.map((item, index) => (
-              <AccordionItem
-                key={index}
-                value={`item-${index}`}
-                className={Styles.accordionItem}
-                style={{ backgroundColor: backgroundColors[index] }}
-              >
-                <AccordionTrigger className={Styles.accordionTrigger}>
-                  {`${item.time}: ${item.activity}`}
-                </AccordionTrigger>
-                <AccordionContent className={Styles.accordionContentContainer}>
-                  <p className={Styles.accordionContentTitle}>
-                    Duration: {item.duration}
-                  </p>
-                  {item.details?.map((detail, i) => (
-                    <section key={i} className={Styles.accordionContent}>
-                      <p>{detail}</p>
+            {agendaItems.map((item, index) => {
+              return (
+                <AccordionItem
+                  key={index}
+                  value={`item-${index}`}
+                  className={Styles.accordionItem}
+                  style={{ backgroundColor: backgroundColors[index] }}
+                >
+                  <AccordionTrigger className={Styles.accordionTrigger}>
+                    {item.time}: {item.activity}
+                  </AccordionTrigger>
+                  <AccordionContent
+                    className={Styles.accordionContentContainer}
+                  >
+                    <section key={index} className={Styles.accordionContent}>
+                      {item.title && (
+                        <div className="flex flex-col gap-6">
+                          {item.title.map((titles, i) => (
+                            <p key={i} className={Styles.accordionContentTitle}>
+                              {titles}
+                            </p>
+                          ))}
+                        </div>
+                      )}
+                      {item.details && (
+                        <div className="flex flex-col gap-4">
+                          {item.details.map((detail, i) => (
+                            <button
+                              key={i}
+                              style={{ backgroundColor: buttonColors[index] }}
+                              className={Styles.accordionContentButton}
+                            >
+                              {detail}
+                            </button>
+                          ))}
+                        </div>
+                      )}
                     </section>
-                  ))}
-                </AccordionContent>
-              </AccordionItem>
-            ))}
+                  </AccordionContent>
+                </AccordionItem>
+              );
+            })}
           </Accordion>
         </section>
       </main>
